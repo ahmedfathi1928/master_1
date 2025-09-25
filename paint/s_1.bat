e0d5f7bf3f9036a358376a3594b3310f78876c7c
ahmedfathi1928 <ahmed.fathi.mahfouz@gmail.com>
2025-09-25 07:00:33 -0700

    test_fathi_3_2

--- paint/s_1.bat
+++ paint/s_1.bat
@@ -1,2 +1,2 @@
 @echo off
-echo Jenkins ran successfully_3_1 at %date% %time% >> C:\Users\AhmedFathy\Desktop\jenkins_success.txt
+echo Jenkins ran successfully_3_2 at %date% %time% >> C:\Users\AhmedFathy\Desktop\jenkins_success.txt
